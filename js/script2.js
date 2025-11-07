d7f048fdc79292478a9ff41e2553b3a42ffc8977
Gretel Alvarez <gretelalvareztang@gmail.com>
2025-11-07 14:45:29 -0500

    imageCarousel4

--- js/script2.js
+++ js/script2.js
@@ -17,10 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
   divCarrusel.appendChild(inner);
 
   const images = [
-    "../img/photos/carousel/02.jpg",
-    "../img/photos/carousel/01.jpg",
-    "../img/photos/carousel/03.jpg"
+    "./img/photos/carousel/02.jpg",
+    "./img/photos/carousel/01.jpg",
+    "./img/photos/carousel/03.jpg"
   ];
+  
 
   images.forEach((src, i) => {
     const item = document.createElement('div');
