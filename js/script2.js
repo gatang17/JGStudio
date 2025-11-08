3f4233513818e72f719db37a490c4bea72e9bb19
Gretel Alvarez <gretelalvareztang@gmail.com>
2025-11-08 12:17:20 -0500

    preload2

--- js/script2.js
+++ js/script2.js
@@ -3,11 +3,11 @@
 window.addEventListener('load', () => {
   const preloader = document.getElementById('preloader');
   preloader.style.opacity = '0';
-  preloader.style.transition = 'opacity 0.5s ease';
+  preloader.style.transition = 'opacity 1.5s ease';
 
   setTimeout(() => {
     preloader.style.display = 'none';
-  }, 500);
+  }, 20000);
 });
 
 
